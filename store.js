eaec7f9bc2ed79d70a909b30d8e726cf5f34af06
Evano Lilian <lilian.evano@gmail.com>
2022-05-10 15:55:51 +0200

    initial commit

---
+++ store.js
@@ -0,0 +1,28 @@
+const store = [
+  "store",
+  {
+    categorys: {
+      hommes: {
+        id: "h",
+        name: "shorts",
+        price: "10",
+        quantity: "8",
+        size: "xs",
+      },
+      femmes: {
+        id: "f",
+        name: "shorts",
+        price: "10",
+        quantity: "8",
+        size: "xs",
+      },
+      enfants: {
+        id: "c",
+        name: "shorts",
+        price: "10",
+        quantity: "8",
+        size: "xs",
+      },
+    },
+  },
+]
